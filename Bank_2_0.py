01e613035a224ac1a8710141b9a7715345ed4533
Memfis <Memfis@gmail.com>
2021-10-30 19:07:57 +0300

    Renamed class(Enter) on Credentials A few minor fixes

--- Bank_2_0.py
+++ Bank_2_0.py
@@ -1,11 +1,12 @@
-
 import random
 import re
 CARDS = []
 CVV = []
 DATE_EXPIRED = []
 MONEY_COUNTER = []
-class Enter:
+
+
+class Credentials:
     def __init__(self):
         self.phone_reg = input('Введите номер мобильного телефона: ')
         if self.phone_reg[:4] != "+375" or len(self.phone_reg) != 13 or not self.phone_reg[1:].isdigit():
@@ -27,7 +28,6 @@ class Enter:
         else:
             print('Введите имя почты на латинском языке.')
 
-
     def login(self, phone_log, password):
         self.phone_log = input('Введите номер мобильного телефона: ')
         if phone_log[:4] != "+375" or len(phone_log) != 13 or not phone_log[1:].isdigit():
@@ -39,24 +39,20 @@ class Enter:
         else:
             print('Введите корректные данные!')
 
-    def check_upper(self,user_input):
+    def check_upper(self, user_input):
         return bool(re.search('[A-Z]', user_input))
 
-    def check_lower(self,user_input):
+    def check_lower(self, user_input):
         return bool(re.search('[a-z]', user_input))
 
-# object_Enter = Enter()
 
-
-
-
-class Card(Enter):
+class Card(Credentials):
 
     def __init__(self):
         super().__init__()
         a = random.randint(1000000000000000, 9999999999999999)
         CARDS.append(a)
-        print('Ваш 16значный номер карточки.', a)
+        print(f'Номер вашей карты:, {a}')
         for i in range(1):
             month = random.randint(1, 12)
             if 1 <= month < 10:
@@ -68,17 +64,16 @@ class Card(Enter):
             print(f'Срок действия вашей карты до {month:0{2}}/{years}')
         self.cvv = random.randint(100, 999)
         CVV.append(self.cvv)
-        print(f'cvv Вашей карты {self.cvv}')
+        print(f'CVV Вашей карты {self.cvv}')
         MONEY_COUNTER.append(0)
 
-class Money:
 
+class Money:
 
     def __init__(self):
         pass
 
-
-    def card_reciver(self):
+    def card_receiver(self):  # зачисление денег на карту
         print("№\t Номер карты\t Срок действия\t  CVV\t Деньги")
         for user in enumerate(CARDS, start=1):
             print(f'{user[0]} {user[1]} \t\t{DATE_EXPIRED[user[0]]}/{DATE_EXPIRED[user[1]]}\t\t\t{CVV[user[0]]} '
@@ -91,14 +86,15 @@ class Money:
             print(f'{user[0]} {user[1]} \t\t{DATE_EXPIRED[user[0]]}/{DATE_EXPIRED[user[1]]}\t\t\t{CVV[user[0]]} '
                   f'\t{MONEY_COUNTER[user[0]]}')
         # Добавить историю
-    def card_sender(self):  #  перевод с карты на карту
+
+    def card_sender(self):  # перевод с карты на карту
         if len(CARDS) >= 2:
             print("№\t Номер карты\t Срок действия\t  CVV\t Деньги")
             for user in enumerate(CARDS, start=1):
                 print(user[0], user[1], "\t\t", DATE_EXPIRED[user[0]], '/', DATE_EXPIRED[user[1]], '\t\t\t',
                       CVV[user[0]], '\t', MONEY_COUNTER[user[0]])
             self.user_choice = int(input('Выберите карту с которой будет осуществлен денежный перевод: '))
-            self.money = int(input("Какая сумма будет переведина с вашей карты: "))
+            self.money = int(input("Какая сумма будет переведена с вашей карты: "))
             MONEY_COUNTER[self.user_choice - 1] -= self.money
             print("№\t Номер карты\t Срок действия\t  CVV\t Деньги")
             for user in enumerate(CARDS, start=1):
@@ -111,18 +107,7 @@ class Money:
             print('Пожалуйста, зарегистрируйте карту получателя.')
 
 
-
-
-
-
-
-
 object_Card = Card()
-
 object_Money = Money()
 object_Card.__init__()
 object_Money.card_sender()
-
-
-
-
